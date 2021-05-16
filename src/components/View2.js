53b05e27a3a6ac5d3759893fca7699e80c5190ff
Arkadiusz Machalica <a.machalica@selleo.com>
2021-05-16 23:10:55 +0200

    Explore behavior of query scoping

--- src/components/View2.js
+++ src/components/View2.js
@@ -3,26 +3,48 @@ import { getUsers } from '../api/users'
 
 const View2 = () => {
   const { data, error, isLoading } = useQuery('users', getUsers)
+  const {
+    data: data2,
+    error: error2,
+    isLoading: isLoading2,
+  } = useQuery('users2', getUsers)
 
-  if (isLoading) {
-    return 'LOADING View 2'
-  }
+  console.log({ isLoading, isLoading2 })
 
   return (
     <div>
       <h1>View 2</h1>
       {error ? (
         <div>error: {error}</div>
+      ) : isLoading ? (
+        'Loading users'
       ) : (
         <ul>
-          {data.data.map((user) => (
-            <li key={user.id} className="UserListItem">
-              User email: {user.email} - {user.firstName} {user.lastName}
-            </li>
-          ))}
+          {data &&
+            data.data.map((user) => (
+              <li key={user.id} className="UserListItem">
+                User email: {user.email} - {user.firstName} {user.lastName}
+              </li>
+            ))}
+        </ul>
+      )}
+      <hr />
+      {error2 ? (
+        <div>error2: {error2}</div>
+      ) : isLoading2 ? (
+        'Loading users2'
+      ) : (
+        <ul>
+          {data2 &&
+            data2.data.map((user) => (
+              <li key={user.id} className="UserListItem">
+                User email: {user.email} - {user.firstName} {user.lastName}
+              </li>
+            ))}
         </ul>
       )}
     </div>
-  )}
+  )
+}
 
-export default View2;
+export default View2
